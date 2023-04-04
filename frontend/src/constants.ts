0cfa155ca250eae4b38dfa8151b8b8724f6332cf
danielmalancioiu <daniel-george.malancioiu@atos.net>
2023-04-04 19:32:22 +0300

    .

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 // const PROD_BACKEND_API_URL = 
 // const DEV_BACKEND_API_URL = "http://localhost:8000/api";
 
-export  const BACKEND_API_URL = "http://localhost:8000/api";
+export  const BACKEND_API_URL = "http://localhost:8080/api";
 // export  const BACKEND_API_URL = "http://dmgbackend-env.eba-mm4q4fpd.eu-north-1.elasticbeanstalk.com/api";
