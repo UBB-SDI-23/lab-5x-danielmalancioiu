08712579351cac07c9f74c77b6900334762b4ffd
danielmalancioiu <daniel-george.malancioiu@atos.net>
2023-04-08 17:36:59 +0300

    port

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,10 +1,10 @@
-const PROD_BACKEND_API_URL = "http://16.171.53.6:8000/api"
-const DEV_BACKEND_API_URL = "http://localhost:8000/api";
+// const PROD_BACKEND_API_URL = "http://16.171.53.6:8000/api"
+// const DEV_BACKEND_API_URL = "http://localhost:8000/api";
 
-//export  const BACKEND_API_URL = "http://16.171.53.6:8000/api";
+export  const BACKEND_API_URL = "http://16.171.53.6:8000/api";
 // export  const BACKEND_API_URL = "http://dmgbackend-env.eba-mm4q4fpd.eu-north-1.elasticbeanstalk.com/api";
 // const PROD_BACKEND_API_URL = "https://dmgbackend-env.eba-mm4q4fpd.eu-north-1.elasticbeanstalk.com/api";
 // const DEV_BACKEND_API_URL = "http://localhost:8000/api";
 
-export const BACKEND_API_URL =
-	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+// export const BACKEND_API_URL =
+// 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
