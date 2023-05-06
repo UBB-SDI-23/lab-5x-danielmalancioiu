65b3e99286bab9a8df4ac3299a555e32af7723db
danielmalancioiu <daniel-george.malancioiu@atos.net>
2023-05-07 00:29:56 +0300

    Lab7 finish

---
+++ backend/src/main/java/dev/dmg/sdi/domain/dto/FlightAllDto.java
@@ -0,0 +1,34 @@
+package dev.dmg.sdi.domain.dto;
+
+import dev.dmg.sdi.domain.entities.Airline;
+import lombok.Data;
+
+@Data
+public class FlightAllDto {
+
+
+	private Long id;
+
+	private String callSign;
+
+	private Integer capacity;
+
+	private String departureAirport;
+
+	private String arrivalAirport;
+
+	private Airline airline;
+
+	private Long numberOfBookings;
+
+	public FlightAllDto(Long id, String callSign, Integer capacity, String departureAirport, String arrivalAirport, Airline airline, Long numberOfBookings) {
+		this.id = id;
+		this.callSign = callSign;
+		this.capacity = capacity;
+		this.departureAirport = departureAirport;
+		this.arrivalAirport = arrivalAirport;
+		this.airline = airline;
+		this.numberOfBookings = numberOfBookings;
+	}
+
+}
