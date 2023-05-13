44220bf67555206bafc09201c4c63d2c6ed3b43b
danielmalancioiu <daniel-george.malancioiu@atos.net>
2023-05-13 16:15:53 +0300

    refresh

--- frontend/src/components/user/ProfilePage.tsx
+++ frontend/src/components/user/ProfilePage.tsx
@@ -51,7 +51,7 @@ export const ProfilePage = () => {
             try{
                 axios.post(`${BACKEND_API_URL}/user/rows-per-page`, userSettings);
                 toast.success(`Rows per page updated to ${number}`);
-                navigate('/airlines');
+                navigate('https://dmg-frontend.netlify.app/airlines');
                 window.location.reload();
             } catch (error: any) {
                 toast.error(error.response.data);
